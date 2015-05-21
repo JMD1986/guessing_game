1309a93f0c4ca3e1d7af7f28b339acccdc93b302
John Daugherty <johndaugherty1986@gmail.com>
2015-05-21 12:36:30 -0400

    added new methods

--- lib/game.rb
+++ lib/game.rb
@@ -4,40 +4,14 @@ class GuessingGame
     @number_of_guesses = 0
     @guessed_numbers = Array.new
     @winning_number = rand(1..100)
-    greetings
 
   end
-  def greetings
-    puts "
 
-      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
-      '''''' ;;;;;;;:    :;;;;;;; ''''''
-      '''''' ;;;;;;   ``   ;;;;;; ''''''
-      '''''' ;;;;:    ;;    :;;;; ''''''
-      '''''' ;;;;     ;;     ;;;; ''''''
-      '''''' ;;;      ;;      ;;; ''''''
-      '''''' ;;;:;;;;;;;;;;;;:;;; ''''''
-      '''''' ;;;  ;;;;;;;;;;  ;;; ''''''
-      '''''' ;;:   ;;;;;;;;   :;; ''''''
-      '''''' ;;;    ;;;;;;    ;;; ''''''
-      '''''' ;;;    ;;;;;;    ;;; ''''''
-      '''''' ;;;    ;;;;;;    ;;; ''''''
-      '''''' ;;;;  ;;`  `;;  ;;;; ''''''
-      '''''' ;;;;, ;      ; ,;;;; ''''''
-      '''''' ;;;;;:        :;;;;; ''''''
-      '''''' ;;;;;;;.    .;;;;;;; ''''''
-      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
-
-
-    Greetings from beautiful North Korea.
-    I am a program made by glorious leader.
-    I am going to choose a number between 1 and 100.
-    you have five chances to guess it before I blow up wyoming.
-    godspeed."
-    puts "\n"
-  end
   def start
     greetings
+    game_play
+  end
+  def game_play
     loop do
       puts "------------------------------------------------------"
       puts "you have used #{@number_of_guesses} out of 5 guesses"
@@ -46,9 +20,7 @@ class GuessingGame
       player_guess_num = gets.chomp.to_i
       puts "------------------------------------------------------"
       if @winning_number == player_guess_num
-        puts "You have saved wyoming from the bombs of my glorious
-        leader. It only took you #{@number_of_guesses} guesses"
-        break
+        winning_screen
       elsif @guessed_numbers.include?(player_guess_num)
         puts "you already said that number"
       elsif player_guess_num > 100 || player_guess_num < 1
@@ -61,7 +33,18 @@ class GuessingGame
       @guessed_numbers << player_guess_num
       @number_of_guesses +=1
       if @number_of_guesses == 5
-        puts "
+        lose_screen
+      end
+      break if @number_of_guesses == 5
+  end
+end
+  def winning_screen
+    puts "Congratulations #{player_name}You have saved wyoming from the bombs of my glorious
+        leader. It only took you #{@number_of_guesses} guesses"
+
+  end
+  def lose_screen
+    puts "
                                           .+m-
                                   `/-`   `smMs
                                 .ohyyhhydmNmm+
@@ -92,10 +75,36 @@ yMs/-:/oy/  oMMMMMm+.
             you lose
      say goodbye to wyoming
         "
-      end
-      break if @number_of_guesses == 5
   end
-end
+  def greetings
+    puts "
+
+      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
+      '''''' ;;;;;;;:    :;;;;;;; ''''''
+      '''''' ;;;;;;   ``   ;;;;;; ''''''
+      '''''' ;;;;:    ;;    :;;;; ''''''
+      '''''' ;;;;     ;;     ;;;; ''''''
+      '''''' ;;;      ;;      ;;; ''''''
+      '''''' ;;;:;;;;;;;;;;;;:;;; ''''''
+      '''''' ;;;  ;;;;;;;;;;  ;;; ''''''
+      '''''' ;;:   ;;;;;;;;   :;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;;  ;;`  `;;  ;;;; ''''''
+      '''''' ;;;;, ;      ; ,;;;; ''''''
+      '''''' ;;;;;:        :;;;;; ''''''
+      '''''' ;;;;;;;.    .;;;;;;; ''''''
+      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
+
+
+    Greetings from beautiful North Korea.
+    I am a program made by glorious leader.
+    I am going to choose a number between 1 and 100.
+    you have five chances to guess it before I blow up wyoming.
+    godspeed."
+    puts "\n"
+  end
 end
 guessinggamenew=GuessingGame.new
 guessinggamenew.start
