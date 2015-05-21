405ffdac0b2d9342d4cc7b75708cc3114b29fc7b
John Daugherty <johndaugherty1986@gmail.com>
2015-05-20 21:02:48 -0400

    added high/low indicators and ascii art.

--- lib/game.rb
+++ lib/game.rb
@@ -7,27 +7,83 @@ class GuessingGame
   end
 
   def start
-    puts "Greetings from beautiful North Korea.
+    puts "
+
+      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
+      '''''' ;;;;;;;:    :;;;;;;; ''''''
+      '''''' ;;;;;;   ``   ;;;;;; ''''''
+      '''''' ;;;;:    ;;    :;;;; ''''''
+      '''''' ;;;;     ;;     ;;;; ''''''
+      '''''' ;;;      ;;      ;;; ''''''
+      '''''' ;;;:;;;;;;;;;;;;:;;; ''''''
+      '''''' ;;;  ;;;;;;;;;;  ;;; ''''''
+      '''''' ;;:   ;;;;;;;;   :;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;    ;;;;;;    ;;; ''''''
+      '''''' ;;;;  ;;`  `;;  ;;;; ''''''
+      '''''' ;;;;, ;      ; ,;;;; ''''''
+      '''''' ;;;;;:        :;;;;; ''''''
+      '''''' ;;;;;;;.    .;;;;;;; ''''''
+      '''''' ;;;;;;;;;;;;;;;;;;;; ''''''
+
+
+    Greetings from beautiful North Korea.
     I am a program made by glorious leader.
     I am going to choose a number between 1 and 100.
     you have five chances to guess it before I blow up wyoming.
     godspeed."
+    puts "\n"
     loop do
+      puts "------------------------------------------------------"
       puts "you have used #{@number_of_guesses} out of 5 guesses"
+      puts "------------------------------------------------------"
       print "what is your guess?: "
       player_guess_num = gets.chomp.to_i
+      puts "------------------------------------------------------"
       if @winning_number == player_guess_num
         puts "You have saved wyoming from the bombs of my glorious
         leader. It only took you #{@number_of_guesses} guesses"
       elsif @guessed_numbers.include?(player_guess_num)
         puts "you already said that number"
-      elsif
-        puts "wrong guess! you must really hate wyoming"
+      elsif player_guess_num > @winning_number
+        puts "wrong guess! you are too high!"
+      elsif player_guess_num < @winning_number
+        puts "Nope! You are too low."
       end
       @guessed_numbers << player_guess_num
       @number_of_guesses +=1
       if @number_of_guesses == 5
-        puts "so sorry. you lose."
+        puts "
+                                          .+m-
+                                  `/-`   `smMs
+                                .ohyyhhydmNmm+
+                               /dsydmmdddmdmMM.
+                               -mmmdyyyyhmNMMmy+.
+                            -odmdhyhhhhdNMMMdyyyys
+                         -+dmdhhyyyhhdNMMMMhyyyyo`
+                      -odmdyyhyyyhhdmNMMMNsoooo:
+                   .odmmhhyyssyyhddNMMMMN-
+                `+dNmhhyysssyhhhmmMMMMMd.
+              :yNmdhyyssssyyyddmNMMMMMs
+           .+mNddyysssossyyddmNNMMMMM+
+         .yNmdhyss+o+ossyhhdmNMMMMMm-
+       .yddyyss+/+/+ooyyhdmNMMMMMMh`
+     `:-`     `.:/++sshddNMMMMMMM+
+    :ysys+/::.`   ./shdmMMMMMMMm-
+   oMmho+///:///.   -hNMMMMMMMs`
+  sNms+/::--:/+/++.  -NMMMMMm:
+ smho/:-```.://+shd+  oMMMMs`
+:-`       `-:/shmNMN` :MMd-
+sso/`     `:ohmNMMMM. om:
+mNs:` `..  `yNMMMMMd-/:
+yMs/-:/oy/  oMMMMMm+.
+`hmhdmNMMs  hMMdo-
+  :sdmMMm-./o:`
+
+            you lose.
+     Say goodbye to wyoming.
+        "
       end
       break if @number_of_guesses == 5
   end
